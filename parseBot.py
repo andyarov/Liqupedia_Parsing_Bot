d9604e101f88072655a5c8203b8ee9148cd48428
9P1xel <re9pixel@gmail.com>
2023-07-19 19:41:31 +0300

    parsing Bot

---
+++ parseBot.py
@@ -0,0 +1,70 @@
+import telebot
+from telebot import types
+from bs4 import BeautifulSoup
+import requests
+
+bot = telebot.TeleBot("telegram.token")
+# create buttons
+category = types.ReplyKeyboardMarkup(resize_keyboard=True)
+apex_button = types.KeyboardButton('Apex Legends')
+valorant_button = types.KeyboardButton('Valorant')
+category.add(apex_button, valorant_button)
+
+@bot.message_handler(commands = ["start"])
+# get start
+def start(message):
+    bot.send_message(message.chat.id, f"Hello, {message.chat.username}, choose a category", reply_markup=category)
+
+@bot.message_handler(content_types = 'text')
+# get category and send answer
+def get_category(message):
+    if message.text == 'Apex Legends':
+        bot.register_next_step_handler(message, s_apex_tier)
+    elif message.text == 'Valorant':
+        bot.register_next_step_handler(message, s_valorant_tier)
+    else:
+        bot.send_message(message.chat.id, f"{message.chat.username}, choose a category", reply_markup=category)
+
+def s_valorant_tier(message):
+    # parsing url, write in a dictionary key = title, value = href
+    # and display website redirect buttons
+    url = 'https://liquipedia.net/valorant/S-Tier_Tournaments'
+    tournaments = {}
+    page = requests.get(url)
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.text, 'html.parser')
+        all_tournaments = soup.findAll('div', class_="gridCell Tournament Header")
+
+        for tag in all_tournaments:
+            tournaments[tag.find_all('a')[1]['title']] = tag.find_all('a')[1]['href']
+
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        button_list = [types.InlineKeyboardButton(f'{key.replace("/", " ", 3)}',
+                                                  url=f"http://liquipedia.net{tournaments[key]}") for key in tournaments]
+        markup.add(*button_list)
+        bot.send_message(message.chat.id, 'tournaments', reply_markup=markup)
+
+
+
+def s_apex_tier(message):
+    # parsing url, write in a dictionary key = title, value = href
+    # and display website redirect buttons
+    url = 'https://liquipedia.net/apexlegends/S-Tier_Tournaments'
+    tournaments = {}
+    page = requests.get(url)
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.text, 'html.parser')
+        all_tournaments = soup.findAll('div', class_="divCell Tournament Header-Premier")
+
+        for tag in all_tournaments:
+            tournaments[tag.find_all('a')[1]['title']] = tag.find_all('a')[1]['href']
+
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        button_list = [types.InlineKeyboardButton(f'{key.replace("/", " ", 3)}',
+                                                  url=f"http://liquipedia.net{tournaments[key]}") for key in tournaments]
+        markup.add(*button_list)
+        bot.send_message(message.chat.id, 'tournaments', reply_markup=markup)
+
+
+
+bot.polling(none_stop=True, interval=0)
