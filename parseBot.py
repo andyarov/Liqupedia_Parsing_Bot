d120f2ef4dc42062066d6b04c28cd7e17c6d5388
9P1xel <re9pixel@gmail.com>
2023-07-19 21:06:06 +0300

    simplified to one function

--- parseBot.py
+++ parseBot.py
@@ -18,46 +18,35 @@ def start(message):
 @bot.message_handler(content_types = 'text')
 # get category and send answer
 def get_category(message):
-    # parsing url, write in a dictionary key = title, value = href
-    # and display website redirect buttons
     if message.text == 'Apex Legends':
-        url = 'https://liquipedia.net/valorant/S-Tier_Tournaments'
-        tournaments = {}
-        page = requests.get(url)
-        # print(page.status_code)
-        if page.status_code == 200:
-            soup = BeautifulSoup(page.text, 'html.parser')
-            all_tournaments = soup.findAll('div', class_="gridCell Tournament Header")
-
-            for tag in all_tournaments:
-                tournaments[tag.find_all('a')[1]['title']] = tag.find_all('a')[1]['href']
-
-            markup = types.InlineKeyboardMarkup(row_width=1)
-            button_list = [types.InlineKeyboardButton(f'{key.replace("/", " ", 3)}',
-                                                      url=f"http://liquipedia.net{tournaments[key]}") for key in
-                           tournaments]
-            markup.add(*button_list)
-            bot.send_message(message.chat.id, 'tournaments', reply_markup=markup)
+        apex_url = 'https://liquipedia.net/apexlegends/S-Tier_Tournaments'
+        apex_class = "divCell Tournament Header-Premier"
+        tournaments(message, apex_url, apex_class)
     elif message.text == 'Valorant':
-        url = 'https://liquipedia.net/apexlegends/S-Tier_Tournaments'
-        tournaments = {}
-        page = requests.get(url)
-        # print(page.status_code)
-        if page.status_code == 200:
-            soup = BeautifulSoup(page.text, 'html.parser')
-            all_tournaments = soup.findAll('div', class_="divCell Tournament Header-Premier")
-
-            for tag in all_tournaments:
-                tournaments[tag.find_all('a')[1]['title']] = tag.find_all('a')[1]['href']
-
-            markup = types.InlineKeyboardMarkup(row_width=1)
-            button_list = [types.InlineKeyboardButton(f'{key.replace("/", " ", 3)}',
-                                                      url=f"http://liquipedia.net{tournaments[key]}") for key in
-                           tournaments]
-            markup.add(*button_list)
-            bot.send_message(message.chat.id, 'tournaments', reply_markup=markup)
+        val_url = 'https://liquipedia.net/valorant/S-Tier_Tournaments'
+        val_class = "gridCell Tournament Header"
+        tournaments(message, val_url, val_class)
     else:
         bot.send_message(message.chat.id, f"{message.chat.username}, choose a category", reply_markup=category)
+
+
+def tournaments(message, url, get_class):
+    tournaments = {}
+    page = requests.get(url)
+    # print(page.status_code)
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.text, 'html.parser')
+        all_tournaments = soup.findAll('div', class_=get_class)
+
+        for tag in all_tournaments:
+            tournaments[tag.find_all('a')[1]['title']] = tag.find_all('a')[1]['href']
+
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        button_list = [types.InlineKeyboardButton(f'{key.replace("/", " ", 3)}',
+                                                  url=f"http://liquipedia.net{tournaments[key]}") for key in
+                       tournaments]
+        markup.add(*button_list)
+        bot.send_message(message.chat.id, 'tournaments', reply_markup=markup)
 
 
 
